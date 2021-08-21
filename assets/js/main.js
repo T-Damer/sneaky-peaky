319e519fc566515f6784c973f5918a691931440d
T-Damer <dpancov64@gmail.com>
2021-08-21 16:58:58 +0300

    Added scroll-header

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,3 +57,9 @@ const scrollActive = () => {
 }
 
 /*===== CHANGE COLOR HEADER =====*/
+window.onScroll = () => {
+  const nav = document.getElementById('header')
+  if (this.scrollY >= 200)
+    nav.classList.add('scroll-header')
+  else nav.classList.remove('scroll-header')
+}
