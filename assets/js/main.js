2bf55c77c8bc5c2b02d9c7d77bf698ec96296f4d
T-Damer <dpancov64@gmail.com>
2021-08-21 14:41:25 +0300

    Added toggle to main.js and css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
-/*===== MENU SHOW =====*/ 
+/*===== MENU SHOW =====*/
+const menuShow = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId)
+  const toggle = document.getElementById(navId)
 
+  if (toggle && nav) {
+    toggle.addEventListener('click', () => {
+      nav.classList.toggle('show')
+    })
+  }
+}
+
+showMenu('nav-toggle', 'nav-menu')
 
 /*===== REMOVE MENU =====*/
 
-
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
 
-
-/*===== CHANGE COLOR HEADER =====*/ 
+/*===== CHANGE COLOR HEADER =====*/
